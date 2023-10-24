2f8db6463a773d94974d76d9188c68267ed24476
Ggyuhwan <133470173+Ggyuhwan@users.noreply.github.com>
2023-10-24 20:50:16 +0900

    [level unrated] Title: 문자열 섞기, Time: 1.26 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String str1, String str2) {
+       	 String answer = "";
+		 
+		 String a = str1 +str2;
+		 for(int i = 0; i<str1.length(); i++) {
+			 answer += str1.substring(i,i+1);
+			 answer += str2.substring(i,i+1);
+			 
+		 }
+	return answer;
+    }
+}
